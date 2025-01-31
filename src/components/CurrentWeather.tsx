da47f39faa5aea8bfdcfbb4a67aea312770a2d55
Rics <danil.klementev.29@gmail.com>
2025-01-31 15:56:53 +0300

    feat: add info widget for current weather

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -18,6 +18,51 @@ const CurrentWeather = () => {
         wind
     } = data;
 
+    const otherInfoWidgets = [
+        {
+            id: 0,
+            icon: 'droplet',
+            name: 'Precipitation',
+            value: Math.round(precipitation.total),
+            unit: 'in/h',
+        },
+        {
+            id: 1,
+            icon: 'wind',
+            name: 'Wind',
+            value: Math.round(wind.speed),
+            unit: 'mph',
+        },
+        {
+            id: 2,
+            icon: 'moisture',
+            name: 'Humidity',
+            value: Math.round(humidity),
+            unit: '%',
+        },
+        {
+            id: 3,
+            icon: 'sunglasses',
+            name: 'UV index',
+            value: Math.round(uv_index),
+            unit: "",
+        },
+        {
+            id: 4,
+            icon: 'clouds-fill',
+            name: 'Clouds cover',
+            value: Math.round(cloud_cover),
+            unit: '%',
+        },
+        {
+            id: 5,
+            icon: 'eye',
+            name: 'Visibility',
+            value: Math.round(visibility),
+            unit: 'mi',
+        },
+    ];
+
     return (
         <div className="CurrentWeather">
             <div className="temperature">
@@ -37,7 +82,23 @@ const CurrentWeather = () => {
                 </div>
             </div>
             <div className="other-infos">
-                
+                {otherInfoWidgets.map(({id, icon, name, value, unit}) => (
+                    <div className="widget" key={id}>
+                        <div className="widget-container">
+                            <div className="info">
+                                <div className="icon">
+                                    <i className={`bi bi-${icon}`}></i>
+                                </div>
+                                <div className="value">
+                                    {value} {unit}
+                                </div>
+                            </div>
+                            <div className="name">
+                                {name}
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
