dd3e524f16ed8b7e891a3c66f61d6ed8d034ada0
Rics <danil.klementev.29@gmail.com>
2025-02-02 00:47:51 +0300

    feat: add dayly forecast widget

--- src/components/DailyForecastWidget.tsx
+++ src/components/DailyForecastWidget.tsx
@@ -1,6 +1,41 @@
-const DailyForecastWidget = ({data}) => {
+import WeatherIcon from "./WeatherIcon.tsx";
+
+type Props = {
+    data: {
+        date: string;
+        icon: number;
+        summary: string;
+        temperature_max: number;
+        temperature_min: number;
+        precipitation: {
+            total: number;
+        }
+    }
+}
+
+const DailyForecastWidget = ({data} : Props) => {
+    const {date, icon, summary, temperature_max, temperature_min, precipitation} = data;
+
     return (
-        <div>DailyForecastWidget</div>
+        <div className="widget">
+            <div className="day">{} </div>
+            <div className="icon-temp">
+                <div className="icon">
+                    <WeatherIcon iconNumber={icon} alt={summary} />
+                </div>
+                <div className="temperature">
+                    <div className="max">
+                        {Math.round(temperature_max)} °C
+                    </div>
+                    <div className="min">
+                        {Math.round(temperature_min)} °C
+                    </div>
+                </div>
+            </div>
+            <div className="precipitation">
+                {Math.round(precipitation.total)} mm/h
+            </div>
+        </div>
     )
 }
 
